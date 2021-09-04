7c42700f40d9d856c1e5f31a5fb169953d53a0b7
CharukaHS <charuka@protonmail.com>
2021-09-04 18:28:07 +0530

    Updated schema to patient reg form inputs

--- backend/routes/schemas/patient.schema.ts
+++ backend/routes/schemas/patient.schema.ts
@@ -1,13 +1,13 @@
 import { Schema } from "express-validator";
 
 const new_patient_schema: Schema = {
-  fname: {
+  firstname: {
     in: "body",
     isString: true,
     errorMessage: "First name is required",
     trim: true,
   },
-  lname: {
+  lastname: {
     in: "body",
     isString: true,
     errorMessage: "Last name is required",
@@ -17,37 +17,173 @@ const new_patient_schema: Schema = {
     in: "body",
     isString: true,
     errorMessage: "Gender is required",
-    trim: true,
+    isIn: {
+      options: [["male", "female"]],
+      errorMessage: "Gender should be male or female",
+    },
   },
-  "dob.d": {
+  dob: {
     in: "body",
-    isInt: true,
+    isString: true,
+    matches: {
+      options: [/\d{4}-\d{2}-\d{2}/], // regex to yyyy-mm-dd
+      errorMessage: "dob should be yyyy-mm-dd format",
+    },
     optional: true,
   },
-  "dob.m": {
+  marital: {
     in: "body",
-    isInt: true,
-    optional: true,
-  },
-  "dob.y": {
-    in: "body",
-    isInt: true,
-    optional: true,
+    isString: true,
+    errorMessage: "Marital status is required",
+    isIn: {
+      options: [["married", "unmarried"]],
+      errorMessage: "Invalid marital status",
+    },
   },
   address: {
     in: "body",
     isString: true,
-    optional: true,
+    trim: true,
+    errorMessage: "Address is required",
   },
-  email: {
+  grama_niladhari: {
     in: "body",
-    isEmail: true,
+    isString: true,
+    trim: true,
     optional: true,
   },
-  tp: {
+  divisional_sector: {
+    in: "body",
+    isString: true,
+    trim: true,
+    optional: true,
+  },
+  contact_number: {
+    in: "body",
+    isMobilePhone: true,
+    errorMessage: "Valid mobile number is required",
+  },
+  phi_tp: {
+    in: "body",
+    isMobilePhone: true,
+    optional: true,
+  },
+  moh_tp: {
+    in: "body",
+    isMobilePhone: true,
+    optional: true,
+  },
+  living_with: {
+    in: "body",
+    isIn: {
+      options: [["alone", "spouse", "sibilings", "children", "relations"]],
+      errorMessage: "Invalid option for living with",
+    },
+  },
+  lw_name: {
+    in: "body",
+    optional: true,
+    isString: true,
+    trim: true,
+  },
+  lw_address: {
+    in: "body",
+    optional: true,
+    isString: true,
+    trim: true,
+  },
+  lw_tp: {
+    in: "body",
+    optional: true,
+    isMobilePhone: true,
+  },
+  edu_status: {
+    in: "body",
+    // TODO: validate possible inputs here
+    isArray: {
+      options: {
+        min: 0,
+        max: 3,
+      },
+    },
+    optional: true,
+  },
+  has_job: {
+    in: "body",
+    isIn: {
+      options: [["true", "false"]],
+      errorMessage: "Job status must be true or false",
+    },
+    toBoolean: true,
+  },
+  job: {
     in: "body",
     isString: true,
     optional: true,
+    trim: true,
+  },
+  gov_facilities: {
+    in: "body",
+    isString: true,
+    optional: true,
+    trim: true,
+  },
+  diseases: {
+    in: "body",
+    isString: true,
+    optional: true,
+    trim: true,
+  },
+  treatment_his: {
+    in: "body",
+    // TODO: validate possible inputs here
+    isArray: {
+      options: {
+        min: 0,
+        max: 3,
+      },
+    },
+    optional: true,
+  },
+  last_clinic_visit: {
+    in: "body",
+    isString: true,
+    matches: {
+      options: [/\d{4}-\d{2}-\d{2}/], // regex to yyyy-mm-dd
+      errorMessage: "Last clinic date should be yyyy-mm-dd format",
+    },
+    optional: true,
+  },
+  informed_over_phone: {
+    in: "body",
+    isString: true,
+    matches: {
+      options: [/\d{4}-\d{2}-\d{2}/], // regex to yyyy-mm-dd
+      errorMessage: "Informed over phone date should be yyyy-mm-dd format",
+    },
+    optional: true,
+  },
+  home_visit: {
+    in: "body",
+    isString: true,
+    matches: {
+      options: [/\d{4}-\d{2}-\d{2}/], // regex to yyyy-mm-dd
+      errorMessage: "Home visit date should be yyyy-mm-dd format",
+    },
+    optional: true,
+  },
+  next_clinic_date: {
+    in: "body",
+    isString: true,
+    matches: {
+      options: [/\d{4}-\d{2}-\d{2}/], // regex to yyyy-mm-dd
+      errorMessage: "Next clinic date should be yyyy-mm-dd format",
+    },
+    optional: true,
+  },
+  hospital_admission: {
+    in: "body",
+    isString: true,
   },
 };
 
