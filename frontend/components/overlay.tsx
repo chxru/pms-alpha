eb6c1188d5eb25839344554a0cd1afefd74629db
CharukaHS <charuka@protonmail.com>
2021-09-04 05:52:10 +0530

    Improved toast message

--- frontend/components/overlay.tsx
+++ frontend/components/overlay.tsx
@@ -1,7 +1,71 @@
 import React from "react";
-import { useToast } from "@chakra-ui/react";
+import { Box, Flex, Icon, Text, useToast } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import {
+  IoIosCloseCircle,
+  IoMdAlert,
+  IoMdCheckmarkCircle,
+} from "react-icons/io";
 
 import NotifyContext from "../contexts/notify-context";
+
+interface AlertCardProps {
+  msg: string;
+  status: "success" | "error" | "warning" | "info";
+  description?: string;
+}
+
+const AlertCard: React.FC<AlertCardProps> = ({ msg, status, description }) => {
+  let icon: IconType, color: string;
+  switch (status) {
+    case "success":
+      icon = IoMdCheckmarkCircle;
+      color = "green.500";
+      break;
+
+    case "info":
+      icon = IoMdAlert;
+      color = "blue.500";
+      break;
+
+    case "warning":
+      icon = IoMdAlert;
+      color = "yellow.400";
+      break;
+
+    default:
+      icon = IoIosCloseCircle;
+      color = "red.500";
+      break;
+  }
+
+  return (
+    <Flex
+      maxW="sm"
+      w="full"
+      mx="auto"
+      bg="white"
+      shadow="md"
+      rounded="lg"
+      overflow="hidden"
+    >
+      <Flex justifyContent="center" alignItems="center" w={12} bg={color}>
+        <Icon as={icon} color="white" boxSize={6} />
+      </Flex>
+
+      <Box mx={-3} py={2} px={4}>
+        <Box mx={3}>
+          <Box color={color} fontWeight="bold">
+            {msg}
+          </Box>
+          <Text color="gray.600" fontSize="sm">
+            {description}
+          </Text>
+        </Box>
+      </Box>
+    </Flex>
+  );
+};
 
 const Overlay: React.FC = ({ children }) => {
   const toast = useToast();
@@ -15,11 +79,11 @@ const Overlay: React.FC = ({ children }) => {
     description?: string;
   }) => {
     toast({
-      title: msg,
-      status,
-      isClosable: true,
       position: "bottom-right",
-      description,
+      // eslint-disable-next-line react/display-name
+      render: () => (
+        <AlertCard msg={msg} status={status} description={description} />
+      ),
     });
   };
 
